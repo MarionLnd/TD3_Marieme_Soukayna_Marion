b731c917efec6d52d1dec5f3f08a4317fbd2c8de
marieme <ps.marieme95@gmail.com>
2020-01-10 18:02:22 +0100

    TD3

---
+++ TD3/server.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+import * as path from "path";
+const app = express();
+
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+app.get("/", (req: any, res: any) => {
+    res.sendFile(path.join(__dirname,'main.html'))
+});
+
+io.on("connection", function (socket: any) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('message', function(message){
+        console.log(message);
+        io.emit('cool', message);
+    });
+
+});
+
+const server = http.listen(3000, function() {
+    console.log("Listening on *:3000");
+});
