cec15904608841ed0966500fb9d025c9070398a7
marieme <ps.marieme95@gmail.com>
2020-01-27 22:10:06 +0100

    TD3

--- TD3/App/server.js
+++ TD3/App/server.js
@@ -10,6 +10,7 @@ app.get("/", function (req, res) {
 });
 io.on("connection", function (socket) {
     console.log('a user connected f');
+    io.emit('Bonjour !');
     var currentUser = {
         'name': '',
         'ssn': ''
