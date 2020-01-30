c10a53f3b7706b2e86e10f279cc740183f1b0261
marieme <ps.marieme95@gmail.com>
2020-01-30 09:45:01 +0100

    TD3

--- TD3/App/server.js
+++ TD3/App/server.js
@@ -1,20 +1,48 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var path = require("path");
+/*import * as express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";*/
+var express = require('express');
+var path = require('path');
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-io.on("connect", function (socket) {
-    console.log('Un utilisateur est connecté');
-    io.sockets.emit('new message', { message: 'Bonjour' });
-    socket.on('deconnexion', function () {
-        console.log('Utilisateur déconnecté');
+var request = require('request');
+// questions to display in chatbox
+var connections = [];
+var data = new Map();
+data.set(0, ' enter your firstname');
+data.set(1, ' enter your lastname');
+data.set(2, ' enter your SSN');
+var cpt = 0;
+var dataMap = new Map();
+var serverResponse = '';
+//log that user was connected  on port 3000 via web socket
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log(' %s sockets is connected', connections.length);
+    io.sockets.emit('new message', { message: data.get(cpt) });
+    socket.on('disconnect', function () {
+        connections.splice(connections.indexOf(socket), 1);
     });
-});
-var server = http.listen(3000, function () {
-    console.log("Listening on *:3000");
+    // get received msg
+    socket.on('sending message', function (message) {
+        console.log('Message is received :', message);
+        // echo the received message back down the
+        io.sockets.emit('new message', { message: ' ** ' + message });
+        cpt++;
+        if (cpt != 3) {
+            io.sockets.emit('new message', { message: data.get(cpt) });
+        }
+        console.log('Message send  :', { message: data.get(cpt) }, ' cpt = ', cpt);
+        if (cpt == 1) {
+            dataMap.set('firstname', message);
+        }
+        if (cpt == 2) {
+            dataMap.set('lastname', message);
+        }
+        if (cpt == 3) {
+            dataMap.set('ssn', message);
+            cpt = 0;
+        }
+    });
 });
--- TD3/App/server.ts
+++ TD3/App/server.ts
@@ -1,23 +1,63 @@
-import * as express from "express";
-import * as path from "path";
+/*import * as express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";*/
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
+const  request = require('request');
 
-app.get("/", (req: any, res: any) => {
-    res.sendFile(path.join(__dirname,'index.html'))
-});
 
-io.on("connect", function (socket: any) {
-    console.log('Un utilisateur est connecté');
-    io.sockets.emit('new message', { message: 'Bonjour' });
 
-    socket.on('deconnexion', function(){
-        console.log('Utilisateur déconnecté');
+// questions to display in chatbox
+const connections = [];
+let data = new Map();
+data.set(0, ' enter your firstname');
+data.set(1, ' enter your lastname');
+data.set(2, ' enter your SSN');
+let cpt = 0;
+
+let dataMap = new Map();
+let serverResponse = '';
+
+//log that user was connected  on port 3000 via web socket
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log(' %s sockets is connected', connections.length);
+    io.sockets.emit('new message', { message: data.get(cpt) });
+
+
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1);
     });
-});
 
-const server = http.listen(3000, function() {
-    console.log("Listening on *:3000");
+    // get received msg
+    socket.on('sending message', (message) => {
+        console.log('Message is received :', message);
+        // echo the received message back down the
+        io.sockets.emit('new message', { message: ' ** ' + message });
+
+        cpt++;
+        if (cpt != 3) {
+            io.sockets.emit('new message', { message: data.get(cpt) });
+        }
+        console.log('Message send  :', { message: data.get(cpt) }, ' cpt = ', cpt);
+
+        if (cpt == 1) {
+            dataMap.set('firstname', message);
+        }
+
+        if (cpt == 2) {
+            dataMap.set('lastname', message);
+        }
+
+        if (cpt == 3) {
+            dataMap.set('ssn', message);
+            cpt = 0;
+        }
+
+    });
 });
