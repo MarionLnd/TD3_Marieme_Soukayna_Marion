40adef539dfe70e5a17ad4253ec00cb7ead4c262
marieme <ps.marieme95@gmail.com>
2020-01-30 10:28:20 +0100

    TD3

--- TD3/App/server.ts
+++ TD3/App/server.ts
@@ -17,7 +17,7 @@ let dataMap = new Map();
 let serverResponse = '';
 
 app.get("/", (req: any, res: any) => {
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.sendFile(path.join(__dirname,'index_.html'))
 });
 
 //log that user was connected  on port 3000 via web socket
