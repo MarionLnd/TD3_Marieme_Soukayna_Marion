10c5a480eadc4c4a47f2adea60b154d37a12732c
marieme <ps.marieme95@gmail.com>
2020-01-31 23:17:56 +0100

    TD3

--- TD3/App/dbconnexion.js
+++ TD3/App/dbconnexion.js
@@ -1,5 +1,10 @@
-const  mongoose  = require("mongoose");
+let  mongoose  = require("mongoose");
 mongoose.Promise  = require("bluebird");
-const  url  =  "mongodb://localhost:27017/person";
+const  url  =  "mongodb://localhost:27017/demo";
 const  connect  =  mongoose.connect(url, { useNewUrlParser: true  });
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB is connected')
+});
+
 module.exports  =  connect;
